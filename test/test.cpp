9d3922df738caf589ec6c4f7545488d069abf10e
Brian Tyler <brian@hackf5.io>
2024-02-25 14:44:19 +0000

    feat: add glm dependency

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,8 @@
+#include <glm/vec3.hpp> // glm::vec3
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+  auto v = glm::vec3(1, 2, 3);
+  EXPECT_EQ(v.x, 1.0f);
 }
