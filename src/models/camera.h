042fb710aa25eb93678bd59a778447a5c05b5227
Brian Tyler <brian@hackf5.io>
2024-03-18 00:05:01 +0000

    feat: slightly better camera

--- src/models/camera.h
+++ src/models/camera.h
@@ -24,28 +24,34 @@ namespace levin
         float m_near;
         float m_far;
 
+        bool m_dirty = true;
+
         struct UniformBlock
         {
             glm::mat4 proj;
             glm::mat4 view;
         } m_uniform_block;
 
-        void update_projection()
+        void update()
         {
+            if (!m_dirty)
+            {
+                return;
+            }
+
             m_uniform_block.proj = glm::perspective(
                 glm::radians(m_fov),
                 m_aspect_ratio,
                 m_near,
                 m_far);
             m_uniform_block.proj[1][1] *= -1;
-        }
 
-        void update_view()
-        {
             m_uniform_block.view = glm::lookAt(
                 m_position,
                 m_target,
                 glm::vec3(0.0f, 0.0f, 1.0f));
+
+            m_dirty = false;
         }
 
     public:
@@ -55,30 +61,29 @@ namespace levin
             const DescriptorSetLayout &descriptor_set_layout);
 
         const glm::vec3 &position() const { return m_position; }
-        glm::vec3 &position() { return m_position; }
+        glm::vec3 &position() { m_dirty = true; return m_position; }
 
         const glm::vec3 &target() const { return m_target; }
-        glm::vec3 &target() { return m_target; }
+        glm::vec3 &target() { m_dirty = true; return m_target; }
 
         float fov() const { return m_fov; }
-        float& fov() { return m_fov; }
+        float &fov() { m_dirty = true; return m_fov; }
 
         float aspect_ratio() const { return m_aspect_ratio; }
-        float& aspect_ratio() { return m_aspect_ratio; }
+        float &aspect_ratio() { m_dirty = true; return m_aspect_ratio; }
 
         float clip_near() const { return m_near; }
-        float& clip_near() { return m_near; }
+        float &clip_near() { m_dirty = true; return m_near; }
 
         float clip_far() const { return m_far; }
-        float& clip_far() { return m_far; }
+        float &clip_far() { m_dirty = true; return m_far; }
 
-        const glm::mat4 &view() const { return m_uniform_block.view; }
-        const glm::mat4 &proj() const { return m_uniform_block.proj; }
+        const glm::mat4 &view() { update(); return m_uniform_block.view; }
+        const glm::mat4 &proj() { update(); return m_uniform_block.proj; }
 
         void flush()
         {
-            update_view();
-            update_projection();
+            update();
             m_uniform_buffer.copy_from(&m_uniform_block, sizeof(UniformBlock));
         }
 
